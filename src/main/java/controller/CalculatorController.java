0072911db039ce5501d07f6ae32a34b69fec5641
soyul <soyul9280@gmail.com>
2025-10-18 12:11:41 +0900

    feat: 구분자만 있는 경우 예외 구현 - 커스텀 구분자 인식 시작 문자열 예외에서 처리됨 - 빈값일 경우 0 반환 기능 추가

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -31,13 +31,16 @@ public class CalculatorController {
                 .separator(":")
                 .operand(0)
                 .build();
+        if (inputedString.equals("")) {
+            return calculator;
+        }
 
         if (isNumber(inputedString)) {
             throw new IllegalArgumentException("구분자를 입력해주세요. 현재: " + inputedString);
         }
 
         if (!inputedString.startsWith("//") && !isNumber(inputedString.substring(0, 1))) {
-            throw new IllegalArgumentException("문자열 선언이 잘못되었습니다. // 혹은 숫자로 시작 가능합니다.");
+            throw new IllegalArgumentException("문자열 선언이 잘못되었습니다. // 혹은 숫자로 시작 가능합니다. " + inputedString);
         }
 
         if (inputedString.startsWith("//")) {
@@ -75,6 +78,10 @@ public class CalculatorController {
 
     public boolean isNumber(String startValue) {
         try {
+            if (startValue.equals("")) {
+                return true;
+            }
+
             Integer.parseInt(startValue);
             return true;
         } catch (NumberFormatException e) {
