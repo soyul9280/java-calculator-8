0a3b60bcb15071f3fc8e647b4be6b9f2b098ca9c
soyul <soyul9280@gmail.com>
2025-10-19 20:10:12 +0900

    refactor: 커스텀구분자 파싱 시작인덱스 수정 - 현상: 공백문자를 구분자로 지정-> 구분자 /로 지정됨 - 해결: 파싱 시작 인덱스를 2로 설정

--- src/main/java/controller/CustomSeparatorService.java
+++ src/main/java/controller/CustomSeparatorService.java
@@ -7,7 +7,7 @@ public class CustomSeparatorService {
     public static String getCustomSeparator(String inputedString) {
         int start = inputedString.indexOf("//");
         int end = inputedString.indexOf("\\n");
-        String customSeparator = inputedString.substring(start + 1, end);
+        String customSeparator = inputedString.substring(start + 2, end);
 
         if (!inputedString.startsWith("\\n", end)) {
             throw new IllegalArgumentException("커스텀 구분자 지정 명령어가 잘못되었습니다.");
