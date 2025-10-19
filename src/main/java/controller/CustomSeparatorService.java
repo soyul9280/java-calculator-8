b8183e00aa194b38d3ac65ae51a441893c31a01b
soyul <soyul9280@gmail.com>
2025-10-19 22:02:05 +0900

    refactor: 커스텀 선언 끝문자 검사 위치 변경 - 현상: \n위치선언하고 잘못된 경우 검사->끝 위치 선언 안됨 - 해결: 위치 선언 전 해당 문자 있는지 검사

--- src/main/java/controller/CustomSeparatorService.java
+++ src/main/java/controller/CustomSeparatorService.java
@@ -6,12 +6,13 @@ public class CustomSeparatorService {
 
     public static String getCustomSeparator(String inputedString) {
         int start = inputedString.indexOf("//");
-        int end = inputedString.indexOf("\\n");
-        String customSeparator = inputedString.substring(start + 2, end);
 
-        if (!inputedString.startsWith("\\n", end)) {
+        if (!inputedString.startsWith("\\n", start + 3)) {
             throw new IllegalArgumentException("커스텀 구분자 지정 명령어가 잘못되었습니다.");
         }
+
+        int end = inputedString.indexOf("\\n");
+        String customSeparator = inputedString.substring(start + 2, end);
 
         return customSeparator;
     }
